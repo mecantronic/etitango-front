0fc18908eb425d3dcd90b90bb7ec0c19940091f5
Francis Lucano Leonel Guzman <FrancisLucanoLeonelGuzman@hotmail.com>
2024-01-19 12:55:27 -0300

    Style: Data-Picker

--- src/components/ETICombo.tsx
+++ src/components/ETICombo.tsx
@@ -30,78 +30,83 @@ export default function ETICombos({setFieldValue, selectedEvent, values }: {
     setOpen(false);
   };
 
-  const newTheme = (theme: any) =>
-    createTheme({
-      ...theme,
-      components: {
-        MuiPickersCalendarHeader: {
-          styleOverrides: {
-            root: {
-              color: '#ffffff',
-              borderRadius: 0,
-              borderWidth: 1,
-              borderColor: '#e91e63',
-              backgroundColor: '#A82548'
-            },
-            switchViewIcon: {
-              color: '#ffffff'
-            }
+const newTheme = (theme: any) =>
+  createTheme({
+    ...theme,
+    components: {
+      MuiPickersCalendarHeader: {
+        styleOverrides: {
+          root: {
+            color: '#ffffff',
+            borderRadius: 0,
+            borderWidth: 1,
+            borderColor: '#e91e63',
+            backgroundColor: '#A82548'
+          },
+          switchViewIcon: {
+            color: '#ffffff'
           }
-        },
-        MuiDayCalendar: {
-          styleOverrides: {
-            weekDayLabel: {
-              color: '#A82548' 
-            }
+        }
+      },
+      MuiDayCalendar: {
+        styleOverrides: {
+          weekDayLabel: {
+            color: '#A82548'
           }
-        },
-        MuiPickersDay: {
-          styleOverrides: {
-            root: ({ selected }: { selected: boolean }) => {
-              return {
-                borderRadius: 15,
-                borderWidth: 1,
-                borderColor: selected ? '#A82548' : 'transparent',
-                border: '1px solid',
-                backgroundColor: selected ? '#A82548' : 'transparent',
-                color: selected ? '#ffffff' : '#A82548',
-                '&:hover': {
-                  backgroundColor: selected ? '#A82548' : '#A82548',
-                  color: selected ? '#ffffff' : '#ffffff'
-                }
-              };
-            },
-            day: {
-              color: '#ffffff'
-            }
-          }
-        },
-        MuiPickersMonth: {
-          styleOverrides: {
-            monthButton: {
-              color: '#ad1457',
+        }
+      },
+      MuiPickersDay: {
+        styleOverrides: {
+          root: ({ selected }: { selected: boolean }) => {
+            return {
               borderRadius: 15,
               borderWidth: 1,
-              borderColor: '#e91e63',
+              borderColor: selected ? '#A82548' : 'transparent',
               border: '1px solid',
-              backgroundColor: '#f48fb1'
-            }
+              backgroundColor: selected ? '#A82548' : 'transparent',
+              color: selected ? '#ffffff' : '#A82548',
+              '&:hover': {
+                backgroundColor: selected ? '#A82548' : '#A82548',
+                color: selected ? '#ffffff' : '#ffffff'
+              }
+            };
+          },
+          day: {
+            color: '#ffffff'
           }
-        },
-        MuiPickersYear: {
-          styleOverrides: {
-            root: {
-              color: '#ffffff',
-              borderRadius: 15,
-              borderWidth: 0,
-              borderColor: '#e91e63',
-              border: '0px solid',
-              backgroundColor: '#A82548'
-            }
+        }
+      },
+      MuiPickersMonth: {
+        styleOverrides: {
+          monthButton: {
+            color: '#ad1457',
+            borderRadius: 15,
+            borderWidth: 1,
+            borderColor: '#e91e63',
+            border: '1px solid',
+            backgroundColor: '#f48fb1'
+          }
+        }
+      },
+      MuiPickersYear: {
+        styleOverrides: {
+          root: {
+            color: '#ffffff',
+            borderRadius: 15,
+            borderWidth: 0,
+            borderColor: '#e91e63',
+            border: '0px solid',
+            backgroundColor: '#A82548'
           }
         }
       }
-    });
+    }
+  })
+
+
+
+
+
 
   const useStyles = makeStyles({
     root: {
