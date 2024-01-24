a36c948af50ce9d42699e786e92f1968e3cef1a6
Francis Lucano Leonel Guzman <FrancisLucanoLeonelGuzman@hotmail.com>
2024-01-24 12:54:49 -0300

    fix: language change

--- src/components/form/DatePickerEdit.tsx
+++ src/components/form/DatePickerEdit.tsx
@@ -1,65 +1,33 @@
-/* eslint-disable prettier/prettier */
 import React from 'react';
 import { DatePicker } from 'formik-mui-x-date-pickers';
 import { makeStyles } from '@mui/styles';
-import { Field } from 'formik';
+import { Field, useField } from 'formik';
+import esLocale from 'date-fns/locale/es';
 
 const CustomSVGIcon = () => (
-  <img src="/img/icon/calendar-add.svg" alt="Custom Icon" width="24" height="24" /> // Usar la ruta a tu SVG externo
+  <img src="/img/icon/calendar-add.svg" alt="Icono personalizado" width="24" height="24" />
 );
 
-// const useStyles = makeStyles({
-//   root: {
-//     '& .MuiOutlinedInput-root': {
-//       '& fieldset': {
-//         borderColor: '#E68650',
-//         borderRadius: '8px',
-//         borderWidth: '2px'   
-//       },
-//       '&:hover fieldset ': {
-//         borderColor: '#E68650',
-//         borderRadius: '8px',
-//       },
-//       '&.Mui-focused fieldset': {
-//         borderColor: '#E68650',
-//         borderRadius: '8px',
-//       },
-//       '& .MuiIconButton-root': { // Estilos para el icono del DatePicker
-//         color: '#A82548', // Cambiar el color del icono aquí
-        
-//       //}
-//       }
-//     }
-
-//   },
-// });
-
-
 export const ETIDatePickerEdit = ({
-  
   fieldName,
   setFieldValue,
   textFieldProps,
   specialCase,
-  borderColor,
-  isDisabled,
-  
+  borderColor
 }: {
   fieldName: string;
-  isDisabled: boolean;
-  // eslint-disable-next-line no-unused-vars
   setFieldValue: (field: string, value: any, shouldValidate?: boolean) => void;
   textFieldProps: any;
   specialCase: boolean;
   borderColor: boolean;
 }) => {
-  
+  const [field] = useField(fieldName);
   const useStyles = makeStyles({
-    root: { 
-       '& .MuiFormHelperText-root': {
+    root: {
+      '& .MuiFormHelperText-root': {
         width: '165px',
-        margin: '2px 0px 0px 2px',
-         },   
+        margin: '2px 0px 0px 2px'
+      },
       '& .MuiOutlinedInput-root': {
         fontFamily: 'inter',
         width: '165px',
@@ -67,51 +35,62 @@ export const ETIDatePickerEdit = ({
         flexDirection: 'row-reverse',
         padding: '2px',
         '& fieldset': {
-          borderColor: specialCase ? 'transparent' : (borderColor ? 'transparent' : 'transparent'),
+          borderColor: field.value ? '#E68650' : '#FDE4AA',
           borderRadius: '8px',
           borderWidth: '1.5px',
           pointerEvents: 'none'
         },
         '&:hover fieldset ': {
-          borderColor: specialCase ? 'transparent' : (borderColor ? 'transparent' : 'transparent'),
+          borderColor: field.value ? '#E68650' : '#FDE4AA',
           borderRadius: '8px',
           pointerEvents: 'none'
         },
         '&.Mui-focused fieldset': {
-          borderColor: specialCase ? 'transparent' : (borderColor ? 'transparent' : 'transparent'),
+          borderColor: field.value ? '#E68650' : '#FDE4AA',
           borderRadius: '8px',
           pointerEvents: 'none'
         },
         '& .MuiOutlinedInput-notchedOutline': {
-          borderColor: specialCase ? 'transparent' : (borderColor ? 'transparent' : 'transparent'),
+          borderColor: field.value ? '#E68650' : '#FDE4AA'
         },
-        '& .MuiIconButton-root': { 
-          color: '#A82548', 
-        },   
-      },
-    },
+        '& .MuiIconButton-root': {
+          color: '#A82548'
+        }
+      }
+    }
   });
 
   const classes = useStyles();
 
-return (
-  <Field
+  return (
+    <Field
       component={DatePicker}
       disablePast
-      disabled={isDisabled}
       textField={{
         ...textFieldProps,
-        className: classes.root, // Agregar las clases al DatePicker
+        className: classes.root,
+        onKeyDown: (e: React.KeyboardEvent<HTMLInputElement>) => {
+          e.preventDefault();
+        }
       }}
       inputProps={{
         style: {
-          fontFamily: 'inter',
-        },
+          fontFamily: 'inter'
+        }
       }}
       name={fieldName}
-      inputFormat="DD-MM-YYYY"
-      inputIcon= {<CustomSVGIcon />}
-      mask="__-__-____"
-      onChange={(value: any) => setFieldValue(fieldName, value.toDate())}
-  />
-)};
+      views={['day', 'month', 'year']}
+      onChange={(value: any) => {
+        console.log('value date aqui -> ', value);
+        if (value && value.toDate) {
+          console.log('value to date ->', value.toDate());
+          setFieldValue(fieldName, value.toDate());
+        } else {
+          console.warn('Fecha no válida');
+          setFieldValue(fieldName, null);
+        }
+      }}
+      locale={esLocale}
+    />
+  );
+};
